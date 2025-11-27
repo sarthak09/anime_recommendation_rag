9435bd2c5fee07646291c0530f4f277acbc3e067
Sarthak <sarthakrawat09@gmail.com>
2025-11-28 01:32:08 +1030

    added requirements file

--- backend/llm.py
+++ backend/llm.py
@@ -2,13 +2,18 @@ from langchain.chat_models import init_chat_model
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-os.environ["GROQ_API_KEY"]=os.getenv("GROQ_API_KEY")
-os.environ["HUGGINGFACEHUB_API_TOKEN"]=os.getenv("HUGGINGFACEHUB_API_TOKEN")
+groq_key = os.getenv("GROQ_API_KEY")
+if groq_key:
+    os.environ["GROQ_API_KEY"] = groq_key
+
+hf_key = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+if hf_key:
+    os.environ["HUGGINGFACEHUB_API_TOKEN"] = hf_key
+
+
 
 class LLM:
     def __init__(self, model_name: str):
-        # Initialize model
         self.llm = init_chat_model(model=model_name)
 
     def llmtest(self, prompt="Hello how are you?"):
